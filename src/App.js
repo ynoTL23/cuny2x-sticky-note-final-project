8a16887fbcb2cd0de4b6de36e17794066d7ae27d
Tony <tonylu23@gmail.com>
2020-01-21 15:04:39 -0500

    record mouse coordinates on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './css/App.css';
 
-function App() {
-  return (
-		<div
-			className='canvas'
-			onClick={() => console.log('canvas clicked')}></div>
-  )
-}
 
-export default App;
+export default class App extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { x: 0, y: 0 }
+	}
+
+	onMouseClick(e) {
+		this.setState({ x: e.screenX, y: e.screenY })
+	}
+
+	render() {
+		const { x, y } = this.state
+
+		return (
+			<div className='canvas' onMouseDown={this.onMouseClick.bind(this)}>
+				<p>X: { x } Y: { y }</p>
+			</div>
+		)
+	}
+}
