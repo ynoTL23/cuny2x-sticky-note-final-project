7f438f0157b0cd380e50a828947d0f3e703e7284
Tony <tonylu23@gmail.com>
2020-01-21 14:09:41 -0500

    create registration form

---
+++ src/Register.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './css/Register.css'
+
+const submitHandler = () => {
+    console.log('registration test success');
+    window.location.reload()
+}
+
+export const Register = () => {
+	const form = (
+        <div className='form'>
+            <h2>Sign Up</h2>
+			<label id='username-label'>Username</label>
+			<input type='text' id='username' required />
+			<br />
+
+			<label id='password-label'>Password</label>
+			<input type='password' id='password' required />
+			<br />
+
+			<div id='submit-btn' onClick={submitHandler}>Submit</div>
+		</div>
+    )
+
+    ReactDOM.render(form, document.getElementById('root'))
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom'
 
 import App from './App'
+import { Register } from './Register'
 // import Login from './Login'
-// import Register from './Register'
 // import PageNotFound from './PageNotFound'
 
 import './css/index.css'
@@ -13,26 +13,25 @@ import * as serviceWorker from './serviceWorker'
 const landing = (
 	<Router>
 		<div className='topbar'>
-			<div class='dropdown'>
-				<div class='dropbtn'>Menu ▼</div>
-				<div class='dropdown-content'>
+			<div className='dropdown'>
+				<div className='dropbtn'>Menu ▼</div>
+				<div className='dropdown-content'>
 					<Link
 						to='/login'
 						onClick={() => console.log('login working')}>
 						Login
 					</Link>
 					<Link
-						to='/register'
-						onClick={() => console.log('register working')}>
+						onClick={Register}>
 						Register
 					</Link>
 				</div>
 			</div>
 		</div>
 		<Switch>
-			{/*
-            <Route exact path='/login' component={Login}/>
+        {/*
             <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login}/>
             <Route component={PageNotFound}/>
         */}
         </Switch>
