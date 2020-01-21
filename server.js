6959129c67b48cc9333671ef0e7ad3d0ec9a709d
danny2590 <dj.encarnacion@gmail.com>
2020-01-21 12:14:39 -0500

    server file configuration setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const db = require('./config/database')
+const db = require('backend/config/database')
 const bodyParser = require('body-parser')
 
 const app = express() 
