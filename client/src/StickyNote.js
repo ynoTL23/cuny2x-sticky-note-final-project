af33569a4a100e23f1150c871fe26cf822f8175b
Tony <tonylu23@gmail.com>
2020-01-22 15:04:01 -0500

    set conditions for creating sticky note

--- client/src/StickyNote.js
+++ client/src/StickyNote.js
@@ -6,19 +6,7 @@ export default class StickyNote extends React.Component {
 	render() {
         const X = this.props.posX
         const Y = this.props.posY
-        console.log(X , Y)
 		return (
-			// <div
-			// 	className='sticky-note'
-			// 	style={{
-			// 		position: 'absolute',
-			// 		left: position.mouseX,
-			//         top: position.mouseY,
-			//         display: 'initial'
-			//     }}>
-			// </div>
-
-
 			<Draggable bounds='.canvas' cancel='textarea' defaultPosition={{x: X, y: Y}}>
 				<div className='sticky-note'>
 					<div className='content'>
