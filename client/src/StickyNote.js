56fc4eb4866b29fb543afba01244d6167dc3a7ed
Tony <tonylu23@gmail.com>
2020-01-22 13:14:09 -0500

    create sticky note on click and draggable

--- client/src/StickyNote.js
+++ client/src/StickyNote.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import Draggable from 'react-draggable'
 import './css/StickyNote.css'
 
 export default class StickyNote extends React.Component {
-    render() {
-        const position = this.props.position
+	render() {
+        const X = this.props.posX
+        const Y = this.props.posY
+        console.log(X , Y)
+		return (
+			// <div
+			// 	className='sticky-note'
+			// 	style={{
+			// 		position: 'absolute',
+			// 		left: position.mouseX,
+			//         top: position.mouseY,
+			//         display: 'initial'
+			//     }}>
+			// </div>
 
-        return (
-			<div
-				className='sticky-note'
-				style={{
-					position: 'absolute',
-					left: position.mouseX,
-                    top: position.mouseY,
-                    display: 'initial'
-                }}>
-            </div>
+
+			<Draggable bounds='.canvas' cancel='textarea' defaultPosition={{x: X, y: Y}}>
+				<div className='sticky-note'>
+					<div className='content'>
+						<textarea
+							className='inputarea'
+                            placeholder='Enter note description here'>
+                        </textarea>
+					</div>
+				</div>
+			</Draggable>
 		)
-    }
-
+	}
 }
