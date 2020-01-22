a4d6882b2a4e27754feb9ef8ae8fcbc5090d620d
danny2590 <dj.encarnacion@gmail.com>
2020-01-22 13:10:37 -0500

    server confirmed, models created

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database.js");
+const notes = require("/notesModel");
+
+const account = db.define(
+  "userAccount",
+  {
+    username: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.TEXT
+    },
+    email: {
+      type: Sequelize.CHAR
+    },
+    accessibleBy: {
+      type: Sequelize.TEXT
+    }
+  },
+  { timestamps: false }
+);
+
+account.hasOne(notes, { foreignKey: "username" });
+
+account.sync().then(function() {
+  console.log("The Notes Table Exists");
+});
